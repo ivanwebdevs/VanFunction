6e9023435f0ea5753b4f08321fc82206cc3e1ae5
Ivan Dwi Yanto <87416763+ivanwebdevs@users.noreply.github.com>
2021-11-25 08:42:40 +0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,18 @@
+<?php
+include'./vanfunction.php';
+
+//Put Database as TXT
+$path = "database.datastore";
+$data = "Data 1
+Data 2
+Data 3
+Data 4";
+isi_database($path,$data)
+//Output : Function will create database.datastore and store data : data 1|Data 2|Data 3|Data 4|||||||
+  
+  
+  
+
+  
+
+?>
